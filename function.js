29830327161b642c6076a53d755814fcc4b711ba
hedgehogz <sheldonwhung@gmail.com>
2025-08-08 00:43:27 -0400

    time

--- function.js
+++ function.js
@@ -39,7 +39,7 @@ function updateTime() {
 
 function updateOpacity() {
     timeGrid.style.opacity = (Math.sin(Date.now() / 500) + 1.25)/2; // Oscillate opacity between .25 and 1.25
-    timeElement.style.left = Math.sin(Date.now() / 500) * 25 + "vw"
+    timeElement.style.left = Math.sin(Date.now() / 500) * 250 + "px"
 }
 
 function loaded() {
